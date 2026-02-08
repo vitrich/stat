2c9a8e1acc46414126a33b1d387b0255a1ebf454
user <dd@22>
2026-02-08 21:27:43 +0300

    lesson3

--- main/migrations/0005_rename_grouphistory_transferdate.py
+++ main/migrations/0005_rename_grouphistory_transferdate.py
@@ -1,9 +1,10 @@
 from django.db import migrations
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("main", "ПРЕДЫДУЩАЯ_МИГРАЦИЯ"),
+        ("main", "0004_remove_student_is_registered_and_more"),
     ]
 
     operations = [
